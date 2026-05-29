3811f19b54ff043965877753c14667732702ee71
Git Sync <git-sync@localhost>
2026-05-28 22:37:44 -0700

    feat(project): project intialization

--- packages/infra/src/events/index.ts
+++ packages/infra/src/events/index.ts
@@ -1,2 +1,2 @@
-export type { AppEvents, AppInternalEvents } from './app-events.js';
-export { createSystemBus } from './create-system-bus.js';
+export type { AppEvents, AppInternalEvents } from './app-events';
+export { createSystemBus } from './create-system-bus';
