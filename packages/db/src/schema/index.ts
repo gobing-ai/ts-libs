1af57ebeee6269d6b76bfa1b8f600b9141f64a37
Git Sync <git-sync@localhost>
2026-06-02 11:47:45 -0700

    feat(project): project intialization

--- packages/db/src/schema/index.ts
+++ packages/db/src/schema/index.ts
@@ -2,4 +2,5 @@ export { index, integer, text } from 'drizzle-orm/sqlite-core';
 export * from './common';
 export { generateCreateTableSql } from './ddl';
 export { type DefinedTable, defineTable } from './define-table';
+export { inboxMessages } from './inbox-messages';
 export { queueJobs } from './queue-jobs';
